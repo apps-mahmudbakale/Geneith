9db3ac897a494c44183f52bce58720e1fc38d30e
apps-mahmudbakale <apps.mahmud.bakale@gmail.com>
2022-10-09 13:42:18 +0100

    WIP

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -192,21 +192,25 @@ class SaleController extends Controller
     public function store(Request $request)
     {
         // dd($request->all());
-        if($sales = Sale::create($request->all())){
-            return response()->json([
-                'success' => true,
-                'message' => 'Beneficiary successfully created.',
-            ]);
-        }
-        // $sales = Sale::create([
-        //     'invoice' => $request->invoice,
-        //     'product_id' => $request->product_id,
-        //     'user_id' => $request->user_id,
-        //     'amount'  => $request->amount,
-        //     'qty' => $request->qty,
-        //     'station_id' => $request->station_id,
-        //     'created_at' => $request->created_at,
-        // ]);
+        // if($sales = Sale::create($request->all())){
+        //     return response()->json([
+        //         'success' => true,
+        //         'message' => 'Beneficiary successfully created.',
+        //     ]);
+        // }
+        $sales = Sale::create([
+            'invoice' => $request->invoice,
+            'product_id' => $request->product_id,
+            'user_id' => $request->user_id,
+            'amount'  => $request->amount,
+            'qty' => $request->qty,
+            'station_id' => $request->station_id,
+            'created_at' => $request->created_at,
+        ]);
+        return response()->json([
+                    'success' => true,
+                    'message' => 'Beneficiary successfully created.',
+                ]);
 
         
 
