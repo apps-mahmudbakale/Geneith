55655e0cce542f2b34b0ec68b16ef9446f32c826
apps-mahmudbakale <apps.mahmud.bakale@gmail.com>
2022-11-07 12:29:02 +0100

    WIP

--- app/Exports/RequestsPerStationSheet.php
+++ app/Exports/RequestsPerStationSheet.php
@@ -27,7 +27,8 @@ class RequestsPerStationSheet implements FromCollection, WithHeadings, WithTitle
             'Approved Quantity',
             'Buying Price',
             'Selling Price',
-            'Total',
+            'Total Sales',
+            'Total Cost',
             'Gross'
         ];
     }
@@ -35,7 +36,7 @@ class RequestsPerStationSheet implements FromCollection, WithHeadings, WithTitle
     public function collection()
     {
        $requestAll = DB::table('requests')
-                ->select('products.name as product', 'requests.request_qty', 'products.buying_price', 'products.selling_price', DB::raw('requests.approved_qty * products.selling_price'), DB::raw('(products.selling_price - products.buying_price) * requests.approved_qty'))
+                ->select('products.name as product', 'requests.request_qty', 'products.buying_price', 'products.selling_price', DB::raw('requests.approved_qty * products.selling_price'), DB::raw('requests.approved_qty * products.buying_price'))
                 ->join('products', 'requests.product_id', '=', 'products.id')
                 ->join('stations', 'requests.station_id', '=', 'stations.id')
                 ->where('requests.station_id', $this->id)
