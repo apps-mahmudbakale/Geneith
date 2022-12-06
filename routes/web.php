cd7d4617c46d0a89878c878aa631f2c4d20eac5b
apps-mahmudbakale <apps.mahmud.bakale@gmail.com>
2022-12-06 12:22:39 +0100

    WIP

--- routes/web.php
+++ routes/web.php
@@ -33,14 +33,14 @@ Route::get('/', function () {
 
 Route::get('/test', function (){
     $report = DB::table('requests')
-    ->selectRaw('DISTINCT products.name as product, requests.approved_qty, products.buying_price, products.selling_price, ABS((requests.approved_qty) - (station_products.quantity)) as sold, station_products.quantity as remaining, products.buying_price * ABS((requests.approved_qty) - (station_products.quantity)), products.selling_price * ABS((requests.approved_qty) - (station_products.quantity)), (products.selling_price * ABS((requests.approved_qty) - (station_products.quantity))) - (products.buying_price * ABS((requests.approved_qty) - (station_products.quantity)))')
-    ->join('products', 'requests.product_id', '=', 'products.id')
-    ->join('stations', 'requests.station_id', '=', 'stations.id')
-    ->join('sales', 'sales.station_id', '=', 'stations.id')
-    ->join('station_products', 'stations.id', '=', 'station_products.station_id')
-    ->where('requests.station_id', '1')
-    ->where('requests.status', 'approved')
-    ->whereRaw('sales.created_at BETWEEN date("2022-10-07") AND date("2022-11-07")')
+        ->selectRaw('DISTINCT products.name as product, requests.approved_qty, products.buying_price, products.selling_price, ABS((requests.approved_qty) - (station_products.quantity)) as sold, station_products.quantity as remaining')
+        ->join('products', 'requests.product_id', '=', 'products.id')
+        ->join('stations', 'requests.station_id', '=', 'stations.id')
+        ->join('sales', 'sales.station_id', '=', 'stations.id')
+        ->join('station_products', 'stations.id', '=', 'station_products.station_id')
+        ->where('requests.station_id', '1')
+        ->where('requests.status', 'approved')
+        ->whereRaw('sales.created_at BETWEEN date("2022-10-07") AND date("2022-11-07")')
     ->toSql();
 
     return $report;
